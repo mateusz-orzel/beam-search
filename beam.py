0981fac09537684b87fd214920819f271a369642
Mateusz Orzeł <mateuszorzel44@gmail.com>
2023-04-25 01:16:09 +0200

    basic beam search

---
+++ beam.py
@@ -0,0 +1,31 @@
+import numpy as np
+import math
+import heapq
+
+vocab = {"A":[0.12, 0.09, 0.82], "B":[0.21, 0.37, 0.92], "C":[0.11, 0.9, 0.01] , "<EOS>": [0.01, 0.11, 0.94]}
+
+def beamSearch(vocab, beam_width, size):
+    
+    arr = [[1, []] for _ in range(len(vocab))]
+
+    for i in range(size):
+        helper = []
+        while arr:
+            p, sentence = arr.pop()
+            for x in vocab.keys():
+                if sentence and sentence[-1] == "<EOS>":
+                    helper.append([p,sentence])
+                else:
+                    helper.append([p*vocab[x][i],sentence+[x]])
+
+        helper.sort()
+        helper[-beam_width:]
+        arr = helper
+
+    return arr[-1]
+
+
+print(beamSearch(vocab, 2, 3))
+
+
+    
